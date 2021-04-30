1d37a77d443d65ad730162c763cedfe7527a1af2
Micheal Ellis <76069321+MichealJWEllis@users.noreply.github.com>
2021-04-30 11:48:01 -0400

    finished lesson 12.3.6 of mod 12

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.get('/api/canidates', (req, res) => {
       return
     }
     res.json({
-        message: 'success',
-        data: rows     
+      message: 'success',
+      data: rows
     })
   })
 })
@@ -89,12 +89,62 @@ app.post('/api/canidates', ({ body }, res) => {
   db.query(sql, params, (err, result) => {
     if (err) {
       res.status(400).json({ error: err.message })
-      return 
-    } 
+      return
+    }
     res.json({
       message: 'success',
       data: body
     })
+  })
+})
+// Get parties table
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * from parties`
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message })
+      return
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    })
+  })
+})
+// Get parties table by id
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`
+  const params = [req.params.id]
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message })
+      return
+    }
+    res.json({
+      message: 'success',
+      data: row
+    })
+  })
+})
+// Delete table by id
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`
+  const params = [req.params.id]
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message })
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Party not found'
+      })
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      })
+    }
   })
 })
 
